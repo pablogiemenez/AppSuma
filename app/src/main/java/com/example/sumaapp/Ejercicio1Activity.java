c722e1cf3572befa9937a3b58813dc6abf2be882
pablogimenez <pabloergimenez@gmail.com>
2024-08-17 10:22:49 -0300

    Agreguo la logica del boton del ejercicio 1 y arranque con el diseño

---
+++ app/src/main/java/com/example/sumaapp/Ejercicio1Activity.java
@@ -0,0 +1,14 @@
+package com.example.sumaapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ejercicio1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio1);
+    }
+}
--- app/src/main/java/com/example/sumaapp/MainActivity.java
+++ app/src/main/java/com/example/sumaapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sumaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Botón Ejercicio 1
+        Button btnEjercicio1 = findViewById(R.id.btnEjercicio1);
+        btnEjercicio1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad Ejercicio1Activity
+                Intent intent = new Intent(MainActivity.this, Ejercicio1Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
